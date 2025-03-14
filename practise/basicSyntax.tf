10e65b28770f364cec67c5bc6feee69eef9f9848
rohanborate6713 <rohanborate453@gmail.com>
2025-03-14 12:27:11 +0530

    first terraform syntax file

---
+++ practise/basicSyntax.tf
@@ -0,0 +1,17 @@
+#basic terraform syntax
+
+# <block> <paramater> {
+#    <arguments>
+# }
+
+
+resource "local_file" "my_file" {
+  filename = "terrform.txt"
+  content = "this is terrform file created automatically by terraform"
+}
+
+
+#resource --> block
+#local_file (resource type) and my_file(resource name) are the  --> parameters
+# filename = "terrform.txt"                --> arguments
+#  content = "this is terrform file"       --> arguments
