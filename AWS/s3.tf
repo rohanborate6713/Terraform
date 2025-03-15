9fad9db2a065c837eda0f6876f7cf48e5875d264
rohanborate6713 <rohanborate453@gmail.com>
2025-03-15 17:33:26 +0530

    bucket name updated

--- AWS/s3.tf
+++ AWS/s3.tf
@@ -2,6 +2,6 @@ resource "aws_s3_bucket" "my_s3_bucket" {
   bucket = "terraform-practice-bucket"
   
   tags = {
-    Name = "terraform-practice-bucket"
+    Name = "terraform-practice-bucket-rb"
   }
 }
