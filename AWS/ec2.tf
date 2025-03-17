c5494d2d90d739a54aae6c2b3e6d94d6e3b06a75
rohanborate6713 <rohanborate453@gmail.com>
2025-03-17 16:03:34 +0530

    meta argument

--- AWS/ec2.tf
+++ AWS/ec2.tf
@@ -1,9 +1,18 @@
 
 resource "aws_instance" "example" {
+
+    #meta argument for the resource block used to create multiple instances
+    #count = 3   
+
+    #for each create instance with different names
+    for_each = toset(["tf1", "tf2", "tf3"])
     ami           = "ami-00bb6a80f01f03502" # Replace with your desired AMI ID
     instance_type = "t2.micro"
+    
 
     tags = {
-        Name = "terraform-instance"
+       # Name = "terraform-instance"
+       #for foreach meta argument
+        Name = each.key
     }
 }
